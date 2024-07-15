999364a018ace1b4e0c22a5ea4e91d800e9392a5
akhileshathamakuri <akhileshathamakuri504@gmail.com>
2024-07-15 18:22:56 +0530

    this is my first project 001

---
+++ src/test/java/com/mysmartindia/project_new_001/AppTest.java
@@ -0,0 +1,20 @@
+package com.mysmartindia.project_new_001;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
